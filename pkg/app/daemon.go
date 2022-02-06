13204a1fef33ce099c0b46e1c1052d43a898d959
Gabe Conradi <gabe.conradi@gmail.com>
2022-02-05 23:08:13 -0500

    add example log event from UISP showing DFS detection

--- pkg/app/daemon.go
+++ pkg/app/daemon.go
@@ -9,6 +9,64 @@ import (
 )
 
 func (a *App) coroutineDFSWatch(ctx context.Context) error {
+	/*
+	   {
+	     "device": {
+	       "authorized": true,
+	       "category": "wireless",
+	       "displayName": "nycmesh-sn3-south",
+	       "firmwareVersion": "8.7.7",
+	       "id": "62351c24-d13b-4e5f-92d9-a39492e718c0",
+	       "ip": "10.xxxx/24",
+	       "mac": "b4:fb:xxx:5c",
+	       "model": "LAP-120",
+	       "modelName": "LiteAP AC",
+	       "name": "nycmesh-sn3-south",
+	       "platformId": "WA",
+	       "platformName": "WA",
+	       "site": {
+	         "id": "xxxx",
+	         "name": "SN3 - 713",
+	         "parent": {
+	           "id": "xxxx",
+	           "name": "Degraw - 2282",
+	           "parent": null,
+	           "status": "active",
+	           "type": "site"
+	         },
+	         "status": "active",
+	         "type": "site"
+	       },
+	       "started": "0001-01-01T00:00:00.000Z",
+	       "type": "airMax",
+	       "updated": "0001-01-01T00:00:00.000Z"
+	     },
+	     "deviceMetadata": {
+	       "alias": "nycmesh-sn3-south"
+	     },
+	     "id": "49205812-6bfe-46b8-8b1a-4b16c0581c12",
+	     "level": "error",
+	     "message": "Device nycmesh-sn3-south main radio changed frequency due to DFS detection",
+	     "site": {
+	       "id": "fd468966-adc9-4f28-b72d-4d97891d6734",
+	       "name": "SN3 - 713",
+	       "parent": {
+	         "id": "d0b4cf55-15a7-4e60-ba4f-973ef74b5d5e",
+	         "name": "Degraw - 2282",
+	         "parent": null,
+	         "status": "active",
+	         "type": "site"
+	       },
+	       "status": "active",
+	       "type": "site"
+	     },
+	     "tags": [
+	       "device",
+	       "device-state"
+	     ],
+	     "timestamp": "2022-02-03T23:11:03.900Z"
+	   },
+	*/
 	log.Printf("dfs watch: start")
 	return fmt.Errorf("implement DFS watching")
 	time.Sleep(15 * time.Second)
