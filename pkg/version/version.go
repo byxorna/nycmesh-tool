9fc7ed4523143273f33e293a2244c71417fdc481
Gabe Conradi <gabe.conradi@gmail.com>
2022-02-07 15:05:14 -0500

    version exports user agent helper

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,8 +1,16 @@
 package version
 
+import (
+	"fmt"
+)
+
 var (
 	GitCommit string = "unknown"
 	GitBranch string = "unknown"
 	Release   string
 	BuildDate string
 )
+
+func UserAgent() string {
+	return fmt.Sprintf("nycmesh-tool/%s-%s", Release, GitCommit)
+}
